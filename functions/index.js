2d69fceff5b4ebef14a2b12171d1d55c91749817
Rajesh Jella <jella.rajesh@gmail.com>
2019-08-21 11:16:02 +0100

    singup & login end-points are added

--- functions/index.js
+++ functions/index.js
@@ -51,6 +51,17 @@ app.post('/tweet', (req, res) => {
     })
 })
 
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regEx)) return true;
+    else return false;
+}
+
 //sign up user
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -58,6 +69,14 @@ app.post('/signup', (req, res) => {
         password: req.body.password,
         handle: req.body.handle
     }
+    let errors = {};
+    if (isEmpty(newUser.email)) errors.email = 'Must not be empty';
+    if (!isEmail(newUser.email)) errors.email = 'Must be a valid email address';
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(403).json(errors);
+
     let userId, token;
     db.doc(`/users/${newUser.handle}`)
         .get().then(doc => {
@@ -102,12 +121,20 @@ app.post('/signup', (req, res) => {
         })
 })
 
+
+
 // login 
 app.post('/login', (req, res) => {
     const user = {
         email: req.body.email,
         password: req.body.password
     }
+
+    let errors = {};
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (!isEmail(user.email)) errors.email = 'Must be a valid email address';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+    if (Object.keys(errors).length > 0) return res.status(403).json(errors);
     firebase.auth().signInWithEmailAndPassword(user.email, user.password)
         .then((data) => {
             return data.user.getIdToken();
